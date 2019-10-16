83f41e9799500f22cf785621d6ffe1e5926149ee
Irving Ivan Corrales Rangel <irving_icr@hotmail.com>
2019-10-16 15:54:13 -0600

    Add Initial migrations

---
+++ Vet.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vet.Web.Data.Entities;
+
+namespace Vet.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
